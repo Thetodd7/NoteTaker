fd99c7db79531ffd9e96f27ce3df1ea2791ba785
Thetodd7 <85806673+Thetodd7@users.noreply.github.com>
2021-07-30 15:00:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,29 @@ const express = require("express");
 const fs = require("fs");
 const path = require('path');
 
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
-
+app.use(express.static('public'));
+//api routes
 require('./routes/routes')(app);
+
+
+
+
+// html routes
+app.get('/notes', function(req,res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get('*', function(req,res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
